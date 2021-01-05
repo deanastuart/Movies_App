7b859c6f504f3e2ac62e94ffad6213bca3d6cfbe
deanastuart <deana.stuart16@gmail.com>
2021-01-04 20:09:14 -0500

    connect to cleardb

--- config.py
+++ config.py
@@ -2,8 +2,15 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-username = os.getenv('MYSQL_user')
-pw=os.getenv('MYSQL')
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + username + ":" + pw + '@localhost/movies'
+# username = os.getenv('MYSQL_user')
+# pw=os.getenv('MYSQL')
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + username + ":" + pw + '@localhost/movies'
+username = 'bb1959290f76e4'
+pw = 'ee34b76f'
+host = 'us-cdbr-east-02.cleardb.com'
+database = '/heroku_d4bc70f96ca7095?reconnect=true'
+SQLALCHEMY_DATABASE_URI = 'mysql://' + username + ":" + pw + host + database
+
+mysql://bb1959290f76e4:ee34b76f@us-cdbr-east-02.cleardb.com/heroku_d4bc70f96ca7095?reconnect=true
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.secret_key ="ZipcodeRocks"
